89d0ec6dbf70fc0367e03d7e412f2b8eef14e437
Duncan Macleod <duncan.macleod@ligo.org>
2021-02-03 22:48:47 +0000

    link with glib

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,6 @@ pushd _build
 # only link libraries we actually use
 export GSL_LIBS="-L${PREFIX}/lib -lgsl"
 export GSTLAL_LIBS="-L${PREFIX}/lib -lgstlal -lgstlaltags -lgstlaltypes"
-export gstreamer_LIBS="-L${PREFIX}/lib -lgstbase-1.0 -lgstreamer-1.0"
 export gstreamer_audio_LIBS="-L${PREFIX}/lib -lgstaudio-1.0"
 export LAL_LIBS="-L${PREFIX}/lib -llal -llalmetaio"
 
